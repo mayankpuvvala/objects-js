ffbfbbffe3f5153ce9146bad0cd97033f3e7e526
MAYANK PUVVALA <104277096+mayankpuvvala@users.noreply.github.com>
2023-01-02 01:56:18 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,17 @@
+let name= "mayank";
+let sec= "j2";
+let roll= 616;
+
+
+
+const course= {
+    name: "mayank",
+    sec: "j2",
+    roll: 616,
+
+    enroll(){
+        console.log("you will be successfull")
+    }
+}
+course.enroll()
+console.log(course)
